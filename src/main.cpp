ae13c83cd2b894eb7695f321e92aa0086cbd33c7
Jeff Mikels <jeffweb@mikels.cc>
2021-03-05 17:18:40 -0500

    working tally

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,11 @@ use the LED as a tally light.
 bool wifi_connected = false;
 
 IPAddress ip;
+WiFiClient tcp;
 AsyncUDP udp;
-int port = 52381;
 int lastclientport = 0;
 IPAddress lastclientip;
+bool pwr_is_on = false;
 
 // memory buffers for VISCA commands
 size_t lastudp_len = 0;
@@ -35,19 +36,25 @@ size_t lastser_len = 0;
 uint8_t lastser_in[16];
 
 // quick use VISCA commands
-uint8_t pwr_on[] = {0x81, 0x01, 0x04, 0x00, 0x02, 0xff};
-uint8_t pwr_off[] = {0x81, 0x01, 0x04, 0x00, 0x03, 0xff};
-uint8_t addr_set[] = {0x88, 0x30, 0x01, 0xff};       // address set
-uint8_t if_clear[] = {0x88, 0x01, 0x00, 0x01, 0xff}; // if clear
+const uint8_t pwr_on[] = {0x81, 0x01, 0x04, 0x00, 0x02, 0xff};
+const uint8_t pwr_off[] = {0x81, 0x01, 0x04, 0x00, 0x03, 0xff};
+const uint8_t addr_set[] = {0x88, 0x30, 0x01, 0xff};       // address set
+const uint8_t if_clear[] = {0x88, 0x01, 0x00, 0x01, 0xff}; // if clear
 
-// quick color commands
-CRGB red = CRGB(0xf0, 0x00, 0x00);
-CRGB yellow = CRGB(0xf0, 0xf0, 0x00);
-CRGB blue = CRGB(0x00, 0x00, 0xf0);
-CRGB green = CRGB(0x00, 0xf0, 0x00);
-CRGB black = CRGB(0x00, 0x00, 0x00);
+// quick color constants
+const CRGB green = CRGB(0xf0, 0x00, 0x00);
+const CRGB yellow = CRGB(0xf0, 0xf0, 0x00);
+const CRGB blue = CRGB(0x00, 0x00, 0xf0);
+const CRGB red = CRGB(0x00, 0xf0, 0x00);
+const CRGB black = CRGB(0x00, 0x00, 0x00);
 CRGB ledcolor = yellow;
-int tallystate = 0;
+
+// tally variables
+int total_inputs = 2;          // vMix always loads with at least two inputs defined
+int tally_input = 1;           // for mental consistency, we use input numbers based on 1 like vMix does
+int tally_status = 0;          // 0: safe, 1: live, 2: preview
+const int tally_bufsize = 100; // vMix can have a lot of inputs
+uint8_t tb[tally_bufsize + 1]; // so there is always a null byte at the end... for printing
 
 // all function definitions
 // remember that C code doesn't do function culling
@@ -113,6 +120,25 @@ void send_visca(const uint8_t *c)
   Serial.println("");
 }
 
+void visca_power(bool turnon)
+{
+  if (turnon)
+  {
+    send_visca(addr_set);
+    delay(500);
+    send_visca(pwr_on);
+    delay(2000);
+    send_visca(if_clear);
+  }
+  else
+  {
+    send_visca(if_clear);
+    delay(2000);
+    send_visca(pwr_off);
+  }
+  pwr_is_on = turnon;
+}
+
 double zoomcurve(int v)
 {
   return ZOOMMULT * pow(v, ZOOMEXP);
@@ -159,7 +185,7 @@ void status()
     Serial.println("-- UDP LISTENING --");
     Serial.print(WiFi.localIP());
     Serial.print(":");
-    Serial.println(port);
+    Serial.println(udp_port);
     Serial.print("tx: G");
     Serial.print(txpin);
     Serial.print(" | rx: G");
@@ -179,31 +205,10 @@ void led(CRGB c)
   ledcolor = c;
 }
 
-void tally(int state)
-{
-  switch (state)
-  {
-  case 0:
-    led(black);
-    break;
-  case 1:
-    led(red);
-    break;
-  case 2:
-    led(blue);
-    break;
-  }
-  tallystate = state;
-}
-void tally()
-{
-  tally(tallystate);
-}
-
 void start_server()
 {
   udp.close(); // will close only if needed
-  if (udp.listen(port))
+  if (udp.listen(udp_port))
   {
     udp.onPacket([](AsyncUDPPacket packet) {
       CRGB oldc = ledcolor;
@@ -236,61 +241,51 @@ void start_server()
   }
 }
 
-void wait_connection()
+int read_tally(int input) { return (tb[8 + input]) - 48; }
+int read_tally() { return read_tally(tally_input); }
+
+void update_tally()
 {
-  while (!wifi_connected)
+  tally_status = read_tally(); // ASCII NUMBERS 0-9 ARE ENCODED 48-57 (0x30-0x39)
+
+  switch (tally_status)
   {
-    wifi_connected = WiFi.isConnected();
-    status();
-    delay(1000);
+  case 0:
+    led(black);
+    break;
+  case 1:
+    led(red);
+    break;
+  case 2:
+    led(blue);
+    break;
   }
-  start_server();
 }
 
-// put your setup code here, to run once:
-void setup()
+void start_tally()
 {
-  M5.begin(true, false, true);
-  led(yellow);
-
-  Serial.begin(baudrate);
-  Serial.println("started...");
-  Serial.println("connecting to WiFi...");
-
-  // start the visca serial connection
-  Serial1.begin(baudrate, SERIAL_8N1, rxpin, txpin, false, 200);
-
-  // connect to WiFi
-  WiFi.setAutoConnect(true);
-  WiFi.begin(SSID, PASSWORD, 0, NULL, true);
-
-  send_visca(addr_set);
-  send_visca(pwr_on);
-
-  M5.dis.drawpix(0, CRGB(0xf0, 0xf0, 0x00));
+  tcp.connect(VMIX_IP, VMIX_PORT);
+  tcp.write("SUBSCRIBE TALLY\r\n");
 }
 
-void loop()
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info)
 {
+  led(green);
+  ip = info.got_ip.ip_info.ip.addr;
+  wifi_connected = true;
+  start_tally();
+  start_server(); // will stop any previous servers
+}
 
-  if (M5.Btn.wasPressed())
-  {
-    Serial.println("button!");
-    Serial.println(tallystate);
-    tally((tallystate + 1) % 3);
-  }
+void WiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info)
+{
+  ip = IPAddress(0, 0, 0, 0);
+  wifi_connected = false;
+}
 
-  // check for wifi connection
-  bool nowconnected = WiFi.isConnected();
-  if (!nowconnected)
-  {
-    led(yellow);
-    wifi_connected = false;
-    wait_connection();
-    tally(0);
-  }
-
-  // check if we have received data over the serial port
+// check if we have received data over the serial port
+void check_serial()
+{
   int available = Serial1.available();
   while (available > 0)
   {
@@ -307,8 +302,105 @@ void loop()
       udp.writeTo(lastser_in, actual, lastclientip, lastclientport);
     Serial.println("");
     available = Serial1.available();
-    tally();
+    update_tally();
   }
+}
+
+void check_tally()
+{
+  int avail = tcp.available();
+  if (avail == 0)
+    return;
+
+  // TALLY DATA ALWAYS LOOKS LIKE THIS:
+  // TALLY OK 120001\r\n
+  // (encoded as ASCII)
+  // input x will be at buffer index 8 + x (because inputs are 1-based)
+  // total inputs will be avail - 9
+  int inputs = avail - 11; // the data will be terminated with \r\n
+  if (inputs > 1 && total_inputs != inputs)
+  {
+    Serial.print("INPUT COUNT HAS CHANGED TO: ");
+    Serial.println(inputs);
+    total_inputs = inputs;
+  }
+
+  // vMix terminates messages with \r\n
+  // readBytesUntil will discard the terminator char
+  // but we also want to discard the \r before it
+  size_t read = tcp.readBytesUntil('\n', tb, tally_bufsize); // never read more than the tally_bufsize
+  if (read == 0)
+    return;
+
+  debug(tb, read);
+  Serial.println("");
+  tb[read - 1] = 0;           // convert the \r to 0 or at least make sure the final char has a null
+  Serial.println((char *)tb); // since there's a null at the end, we can print the buffer like a string
+
+  update_tally();
+}
+
+// put your setup code here, to run once:
+void setup()
+{
+  M5.begin(true, false, true);
+  led(yellow);
+
+  Serial.begin(baudrate);
+  Serial.println("started...");
+  Serial.println("connecting to WiFi...");
+
+  // connect to WiFi
+  WiFi.setAutoConnect(true);
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(WiFiDisconnected, WiFiEvent_t::SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.begin(SSID, PASSWORD, 0, NULL, true);
+
+  // start the visca serial connection
+  Serial.println("connecting to camera...");
+  Serial1.begin(baudrate, SERIAL_8N1, rxpin, txpin, false, 200);
+
+  visca_power(true);
+}
+
+void loop()
+{
+  if (M5.Btn.wasPressed())
+  {
+    tally_input = (tally_input % total_inputs) + 1; // mod before add yields 1-indexed values
+    Serial.println("button!");
+    Serial.print("WATCHING INPUT: ");
+    Serial.println(tally_input);
+    update_tally();
+  }
+  else if (M5.Btn.pressedFor(750))
+  {
+    Serial.println("long press button!");
+    // find the next active tally by cycling through all the inputs to see
+    // if any of them are preview
+    for (int i = 0; i < total_inputs; i++)
+    {
+      tally_input = (tally_input % total_inputs) + 1; // mod before add yields 1-indexed values
+      if (read_tally() == 2)
+        break;
+    }
+    Serial.print("WATCHING INPUT: ");
+    Serial.println(tally_input);
+    update_tally();
+  }
+
+  // blink if no wifi connection
+  if (!wifi_connected)
+  {
+    led(black);
+    delay(100);
+    led(yellow);
+    delay(100);
+    return;
+  }
+
+  check_serial();
+  check_tally();
 
   // check if we have received data over the tally tcp port
   // later...
